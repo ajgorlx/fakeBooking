1bd366dff4d9f9271bd583060a081f5b043ddce7
Igor Prus <igorprus42@gmail.com>
2024-02-23 02:27:46 +0100

    change class App to function App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header'
 import Menu from './components/Menu/Menu'
 import Hotels from './components/Hotels/Hotels'
 import './index.css';
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingIcon from './components/UI/LoadingIcon/LoadingIcon';
 import Searchbar from './components/UI/Searchbar/Searchbar'
 import Layout from './components/Layout/Layout';
@@ -13,91 +13,85 @@ import ThemeContext from './components/Context/themeContext';
 import AuthContext from './components/Context/authContext';
 
 
-class App extends Component {
-  hotels=[
-    {
-      id: 1,
-      name: 'Pod górami',
-      city: 'Zakopane',
-      rating: 8.3,
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image:''
-    },
-    {
-      id: 2,
-      name: 'Za lasami',
-      city: 'Karpacz',
-      rating: 8.9,
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image:''
-    }
-  ]
-    state = {
-    hotels: [],
-    loading: true,
-    theme: 'primary',
-    isAuthenticated: false
+const backendHotels = [
+  {
+    id: 1,
+    name: 'Pod górami',
+    city: 'Zakopane',
+    rating: 8.3,
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image:''
+  },
+  {
+    id: 2,
+    name: 'Za lasami',
+    city: 'Karpacz',
+    rating: 8.9,
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image:''
+  }
+]
+
+
+function App() {
+
+  const [hotels, setHotels] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [theme, setTheme] = useState('primary');
+  const [isAuthenticated, setIsaAuthenticated] = useState(false);
+  
+  const changeTheme = () => {
+    const newTheme = theme === 'primary' ? 'warning' : 'primary'
+    setTheme(newTheme)
+  }
+
+  const searchHandler = term =>{
+    console.log('szukaj z app', term)
+      const newHotels = [...backendHotels]
+        .filter(x => x.name
+          .toLowerCase()
+          .includes(term.toLowerCase()))
+    setHotels(newHotels);
   };
 
-  searchHandler(term){
-    console.log('szukaj z app', term)
-    const hotels = [...this.hotels]
-                    .filter(x => x.name.toLowerCase().includes(term.toLowerCase()))
-    this.setState({ hotels });
-  }
-  componentDidMount(){
+useEffect(() =>{
     setTimeout(() => {
-      this.setState({
-        hotels: this.hotels,
-        loading: false
-      });
+     setHotels(backendHotels);
+     setLoading(false);
     }, 1000);
-    console.log("komponent zamontowany")
-  }
+  },[])
 
-  changeTheme = () => {
-    const newTheme = this.state.theme === 'primary' ? 'warning' : 'primary'
-    this.setState({ theme: newTheme })
-  }
 
-  render(){
+  const header = (
+  <Header>
+    <Searchbar 
+    onSearch={term => searchHandler(term)}/>
+     <ThemeButton />
+   </Header>
+   )
+   const menu = (
+     <Menu/>
+   )
+   const content = (
+   loading 
+   ? <LoadingIcon/>
+   : <Hotels 
+   hotels={hotels}/>
+   )
+   const footer = (
+     <Footer />
+   )
 
-    const header = (
-     <Header>
-     <Searchbar 
-     onSearch={term => this.searchHandler(term)} 
-    
-     />
-    <ThemeButton />
-    </Header>
-    )
-    
-    const menu = (
-      <Menu/>
-    )
+return (
 
-    const content = (
-    this.state.loading 
-    ? <LoadingIcon/>
-    : <Hotels 
-    hotels={this.state.hotels}
-  
-     />
-    )
-
-    const footer = (
-      <Footer />
-    )
-    
-  return (
-    <AuthContext.Provider value=
-    {{ isAuthenticated: this.state.isAuthenticated,
-    login: () => this.setState ({ isAuthenticated:true }),
-    logout: () => this.setState ({ isAuthenticated:false })
+<AuthContext.Provider value=
+    {{ isAuthenticated: isAuthenticated,
+    login: () => setIsaAuthenticated(true),
+    logout: () => setIsaAuthenticated(false)
     }}>
     <ThemeContext.Provider value={{
-      color: this.state.theme,
-      changeTheme: this.changeTheme
+      color: theme,
+      changeTheme: changeTheme
       
       }}>
     <Layout 
@@ -108,8 +102,7 @@ class App extends Component {
     />
     </ThemeContext.Provider>
     </AuthContext.Provider>
-  );
-  }
-}
+);
+};
 
 export default App;
