167f81fdd8d3206840dfa0c559b7520746b0f616
Igor Prus <igorprus42@gmail.com>
2024-03-04 11:27:29 +0100

    added validations on addHotel

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -2,3 +2,35 @@ export function validateEmail(text){
     const re = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
     return re.test(text)
     }
+
+    const availableRules = {
+        required(value) {
+            return value ? '' : 'Pole wymagane';
+        },
+        min(value, rule){
+            return value.length < rule.length ? '' : `Min. ${rule.length} znaki`
+        }
+        
+    }
+
+export function validate (rules =[], value){
+    for (let i = 0 ; i < rules.length ; i++){
+        const rule = rules[i]
+    
+
+        if(rule instanceof Object ){
+            const errorMessage = availableRules[rule.rule](value, rule)
+            if (errorMessage){
+                return errorMessage;
+            }
+        } else {   
+          const errorMessage = availableRules[rule](value)
+            if (errorMessage){
+                return errorMessage;
+        }
+        }
+    }
+    
+
+    return '' 
+}
--- src/pages/Profile/MyHotels/AddHotel.js
+++ src/pages/Profile/MyHotels/AddHotel.js
@@ -2,43 +2,43 @@ import "./AddHotel.css"
 import LoadingButton from "../../../components/UI/LoadingButton/LoadingButton";
 import { useRef, useState } from "react";
 import Input from "../../../components/Input/Input";
+import { validate } from "../../../helpers/validations";
 
 const AddHotel = props => {
-    const imageRef = useRef()
     const [form, setForm] = useState({
     name:{
         value: '',
         error: '',
         showError: false,
-        rules: ['required']
+        rules: ['required', {rule: 'min', length: 4}]
     },
     description:{
         value: '',
         error: '',
         showError: false,
+        rules: ['required', {rule: 'min', length: 10}]
     },
     city:{
         value: '',
         error: '',
         showError: false,
-        rules: ['required']
+        rules: ['required', {rule: 'min', length:2}]
     },
     rooms:{
         value: 2,
         error: '',
         showError: false,
-        rules: ['required']
+        rules: ['required', {rule: 'min', length:1}]
     },
     features:{
-        value: null,
-        error: '',
-        showError: false,
-    },
-    image:{
         value: [],
         error: '',
-        showError: false,
-        rules: ['required']
+        showError: false
+    },
+    image:{
+        value: null,
+        error: '',
+        showError: false
     },
     status:{
         value: 0,
@@ -58,6 +58,19 @@ const AddHotel = props => {
     }, 500);
     }
 
+    const changeHandler = (value, fieldName) => {
+        const error = validate(form[fieldName].rules, value);
+      setForm({
+        ...form, 
+        [fieldName]: {
+            ...form[fieldName],
+            value,
+            showError: true,
+            error: error
+        }
+         })
+    }
+
 
     return (
 <div className="card">
@@ -74,69 +87,72 @@ const AddHotel = props => {
                 <Input 
                     label="Nazwa"
                     value={form.name.value}
-                    onChange={value => setForm({...form, name: value})}
-                    error=""
-                    showError={false} />
+                    onChange={val => changeHandler(val, "name")}
+                    error={form.name.error}
+                    showError={form.name.showError} />
 
                 <Input 
                     label="Opis"
                     type="textarea"
                     value={form.description.value}
-                    onChange={value => setForm({...form, description: value})}
-                    error=""
-                    showError={false} />
+                    onChange={val => changeHandler(val, "description")}
+                    error={form.description.error}
+                    showError={form.description.showError} />
 
                 <Input 
                     label="Miejscowość"
                     value={form.city.value}
-                    onChange={value => setForm({...form, city: value})}
-                    error=""
-                    showError={false} />
+                    onChange={val => changeHandler(val, "city")}
+                    error={form.city.error}
+                    showError={form.city.showError} />
                 
                 <Input 
                     label="Ilośc pokoi"
                     value={form.rooms.value}
                     type="select"
-                    onChange={value => setForm({...form, rooms: value})}
+                    onChange={val => changeHandler(val, "rooms")}
                     options ={[
                         {value: 1, label: 1},
                         {value: 2, label: 2},
                         {value: 3, label: 3},
                         {value: 4, label: 4},
                     ]}
-                    error=""
-                    showError={false} />
+                    error={form.rooms.error}
+                    showError={form.rooms.showError} />
 
                 <h4>Udogodnienia</h4>
                 <Input 
                     type="checkbox"
                     value={form.features.value}
-                    onChange={value => setForm({...form, features: value})}
+                    onChange={val => changeHandler(val, "features")}
                     options={[
                         {value:"tv", label:"TV"},
                         {value:"wifi", label:"WiFi"},
                         {value:"parking", label:"Parking"},
                     ]}
-                    error=""
-                    showError={false} />
+                    error={form.features.error}
+                    showError={form.features.showError} />
 
                 <h4>Zdjęcia</h4>
                 <Input 
                     type='file'
-                    onChange={value => setForm({...form, image: value})}/>
+                    onChange={val => changeHandler(val, "image")}
+                    error={form.image.error}
+                    showError={form.image.showError}
+                    />
            
                 <h4>Status</h4>
                 <Input 
                     type="radio"
                     name="status"
                     value={form.status.value}
-                    onChange={value => setForm({...form, status: value})}
+                    onChange={val => changeHandler(val, "status")}
                     options={[
                         {value:"1", label:"Aktywny"},
                         {value:"0", label:"Ukryty"},
                     ]}
-                    error=""
-                    showError={false} />
+                    error={form.status.error}
+                    showError={form.status.showError} />
 
         <div className="text-right">
             <LoadingButton
